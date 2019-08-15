13cea73e1e6a387b108846c166271c1abc3db307
Denis Bystruev <github@learnSwift.ru>
2019-08-15 22:38:32 +0300

    Added requests to NASA APOD site

--- HTTP Session/ViewController.swift
+++ HTTP Session/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadRequest()
     }
 
+    func loadRequest() {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                print(#function, #line, error?.localizedDescription ?? "no description")
+                return
+            }
+            
+            guard let stringData = String(data: data, encoding: .utf8) else {
+                print(#function, #line, "ERROR: can't decode \(data)")
+                return
+            }
+            
+            print(stringData)
+        }
+        task.resume()
+    }
 
 }
 
